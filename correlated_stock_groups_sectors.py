af07812e69963e5d6ca0147076e175d089321efc
dbourou <annabd24@yahoo.com>
2023-10-27 17:55:32 +0200

    cleanup and commments

--- correlated_stock_groups_sectors.py
+++ correlated_stock_groups_sectors.py
@@ -1,33 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct 18 13:14:53 2023
+Created on Fri Oct 27 17:00:00 2023
 
 @author: Dimitra Bourou
 """
 
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 16 10:42:39 2023
-
-@author: Dimitra Bourou
-"""
-
-import bs4 as bs # to crawl data from webpages
-import pickle # to save python variables
-import requests
 import datetime as dt
 import os
 import pandas as pd
-import pandas_datareader.data as web
 import yfinance as yf
 import numpy as np
-import pickle
 import matplotlib
 matplotlib.use('QtAgg')
-import sys
-import pylab
 import matplotlib.pyplot as plt
 from matplotlib import style
 
@@ -52,39 +37,20 @@ style.use('ggplot')
 
 
 # get some common lists of stocks, get also company data
-# we can check if companies which correlate are always from same sectors
-dow = si.tickers_dow()
-nasdaq = si.tickers_nasdaq()
+# we can later check if companies which correlate are also from same sectors
 other = si.tickers_other(include_company_data=True) # but idk which stockexchange they are from, get company info
+# other = si.tickers_dow()
+# other = si.tickers_nasdaq()
+# other = si.tickers_sp500()
+# to get tickers from another stock exchange, uncomment one of the previous 3 lines
+# the script might then require small modifications
 
-sp500 = si.tickers_sp500()
+# there are many different versions of the tickers
+# for simplicity just keep companies that have the same ticker in all 3, they are the majority
 
-mystocks = ['biib', 'regn', 'aixa', 'una', 'meta', 'baba', 'vnm', 'ech' ]
-
-tobuy = ['ewm','thd', 'givn' ]
-# malaysian etf, thai etf, givadaun
-
-
-# tickers = list(other['']
- # dow / nasdaq / other / sp500 / mystocks / tobuy
-
-#tickers = tickers[0:100]
-
-
-#columns = ['ACT Symbol', 'Security Name', 'Exchange', 'CQS Symbol', 'ETF',
-#       'Round Lot Size', 'Test Issue', 'NASDAQ Symbol']
-
-a1 = other['ACT Symbol'] == other['NASDAQ Symbol']
-a2 = other['ACT Symbol'] == other['CQS Symbol']
-a3 = other['CQS Symbol'] == other['NASDAQ Symbol']
-
-a11 = a1.value_counts()
-a21 = a2.value_counts()
-a31 = a3.value_counts()
-
-# there are many tickers... which one to use?
-#tickers = list(other['']
-# for simplicity just keep those that have the same ticker in all 3, they are the majority
+a1 = ( other['ACT Symbol'] == other['NASDAQ Symbol'] )
+a2 = ( other['ACT Symbol'] == other['CQS Symbol'] )
+a3 = ( other['CQS Symbol'] == other['NASDAQ Symbol'] )
 
 other = other.loc[(a1 & a2 & a3)] 
 other = other[['ACT Symbol', 'Security Name']]
@@ -92,11 +58,39 @@ other.rename(columns={'ACT Symbol': 'Ticker'}, inplace=True)
 
 tickers = list(other['Ticker'].unique())
 
-# find sector info with some other command
+##### REMOVE THIS AFTER
+tickers = tickers[0:200]
+
+# get the historical stock price data, and save them locally (this will take some time)
+# make also an "outputs folder" to store this and other data
+
+# first check if the directory exists to save the outputs
+isExist = os.path.exists('outputs')
+if not isExist:
+   # Create a new directory because it does not exist
+   os.makedirs('outputs')
+   
+# first check if the directory exists to save the stock data
+path = os.path.join('outputs', 'stock_dfs')
+isExist = os.path.exists(path)
+if not isExist:
+      # Create a new directory because it does not exist
+      os.makedirs(path)
+      
+      
+# set current folder to "outputs" so that CSVs we generate get stored there
+
+from pathlib import Path
+
+root_dir = Path(__file__).resolve().parent
+path_out = os.path.join(root_dir, 'outputs')
+os.chdir(path_out)
+cd = os.getcwd()
+
+# download the stock data locally
 
 start = dt.datetime(2015,1,1)
 end = dt.datetime(2023,1,1)
-   
 
 for ticker in tickers: # take all tickers later, will take a few mins
     print(ticker)    
@@ -106,7 +100,6 @@ for ticker in tickers: # take all tickers later, will take a few mins
     else:
         print('Already have {}'.format(ticker))
 
-#maybe let it skip a ticker if its empty or not found
 
 def compile_data():
 
@@ -139,7 +132,7 @@ df = pd.read_csv('joined_data_other.csv')
 
 df = df[df['Date'] > '2015-01-01']
 
-df.dropna(axis=1, how="any", inplace=True) #, thresh=None, subset=None, inplace=False)
+df.dropna(axis=1, how="any", inplace=True)
 
 
 stock_names = list(df.columns)
@@ -147,44 +140,26 @@ stock_names.pop(stock_names.index('Date'))
 
 tickers = stock_names
 
-#df['AAPL'].plot()
-#plt.show()
-df_corr = df.corr() # VERY WELL PAID INFO! informs stock picking
-# could do this with a delay as well and get more results
+# get the covariance matrix
+df_corr = df.corr()
 
-
-
-print(df_corr.head())
-
-
-# save stock tickers and turn df into numpy array
-#stock_names = df_corr.columns.tolist()
+# turn df into numpy array
 
 corr = df_corr.to_numpy()
 
-# turn to array, save company names in order and find the locations
-# of the values that are close to zero, etc and return the pairs
-# turn to sets if I want groups
-# np.where(a == 3)[0]
+# plot a distribution of correlation values 
+# to see how the stocks are distributed in terms of how correlated they are
+# and to see which cutoffs make sense (i.e., if almost all have 0.8 correlation
+# but very few have 0.85 then thats a good threshold)
 
-# plot a distribution of values so I can see which cutoffs make sense
-# organize into pairs and 
-
+# first replace the diagonal values with nans, we don't want those
 n = corr.shape[0]
 corr[range(n), range(n)] = np.nan
 
-# no need for pairs, just remove 1 values from diagonal
-# and then flatten and plot
-
+# flatten the covariance matrix (we just want the values) and make a barplot
 corr_flat = np.reshape(corr, [1,n*n])
 # maybe remove nans
 corr_flat = corr_flat[~np.isnan(corr_flat)]
-
-
-#plt.hist(corr_flat, bins = [ -1, -0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1 ])
-#plt.show()
-#plt.savefig('histogram_correlations.png', dpi=1000)
-
 
 num_bins = 10
 
@@ -198,44 +173,26 @@ fig.tight_layout()
 plt.show()
 plt.savefig('histogram_correlations.png', dpi=1000)
 
-
-
-
-
-uncorr_inds = np.where((corr > -0.2) & (corr < 0.2))
+# find the indices in the covariance matrix where cov > 0.95
 pos_cor_inds = np.where(corr > 0.95) 
-# 0.75-0.85 makes all of them correlated, at least the sp500 ones. make it stricter
-# 0.95 shows some groups. but try different than sp500, maybe these are all highly correlated
-# or play around more with other stuff like correlating fewer years
-neg_cor_inds = np.where(corr < -0.75)
 
-
-uncorr_x = list(uncorr_inds[0])
-uncorr_y = list(uncorr_inds[1])
-uncorr_pairs = [[stock_names[x],stock_names[y]] for (x,y) in zip(uncorr_x,uncorr_y) ]
-print('uncor_pairs len: ', len(uncorr_pairs))
-
+# save the tickers of the correlated stocks as a list of pairs
 pos_cor_x = list(pos_cor_inds[0])
 pos_cor_y = list(pos_cor_inds[1])
 pos_cor_pairs = [[stock_names[x],stock_names[y]] for (x,y) in zip(pos_cor_x,pos_cor_y) ]
 print('pos cor pairs len: ', len(pos_cor_pairs))    
 
-neg_cor_x = list(neg_cor_inds[0])
-neg_cor_y = list(neg_cor_inds[1])
-neg_cor_pairs = [[stock_names[x],stock_names[y]] for (x,y) in zip(neg_cor_x,neg_cor_y) ]
-print('neg cor pairs len: ', len(neg_cor_pairs))   
-
-# how to reduce these lists to groups? transitive and symmetric property apply
-# removing symmetric pairs is easy
-
+# however, positive correlation is a symmetric and transitive property,
+# meaning that if A corr with B and B with C we have a group ABC of corr stocks
+# to get these groups we can use network theory
+# each ticker pair is a node in a graph and we can find connected components
 
 import networkx
 
 samples = pos_cor_pairs
 
 samples = [pair for pair in pos_cor_pairs if pair[0] != pair[1]]
-# eliminate the pairs of stocks correlated with themselves
-
+# exclude the pairs of stocks correlated with themselves
 
 ga = networkx.Graph(samples)
 conn_comp = networkx.connected_components(ga)
@@ -247,7 +204,6 @@ for subgraph in networkx.connected_components(ga):
 
 # now turn this into a dataframe with an index for groups, and then find their sectors
 
-# turn list of groups into list of lists
 groups_list = [list(group) for group in groups]
 
 grouped_stocks = []
@@ -264,32 +220,21 @@ for i in range(0,len(groups_list)):
         grouped_stocks = grouped_stocks + whole_group
 
 
-#import itertools
-#grouped_stocks_unpacked = list(itertools.chain.from_iterable(grouped_stocks))
-
-
-# save this offline, takes a long time to find subgroups
-
-#tickers = [item[0] for item in grouped_stocks]  
-#tickers2 = Ticker(tickers)
-
+# save tickers and their groups into a CSV, because the network algorithm can take a long time to run
 data_other = pd.DataFrame(grouped_stocks, columns=['Ticker', 'Corr group']) 
 
 data_other.to_csv('other_stocks_grouped.csv')
-
-
 
 data_other = pd.read_csv('other_stocks_grouped.csv')
 
 data_other.drop(columns=['Unnamed: 0'], inplace=True)
 
 
-#tickers = list(data_other['Ticker'].unique())
+# now get the information about the countries and the sectors of the companies we grouped
+# we want to see if the groups formed have anything to do with sector or country
 
-#tickers2 = Ticker(tickers)
-
-
-
+# lets create a dictionary with the fields we are interested in
+# we will later turn it into a dataframe
 dictionary = {'Ticker':[],
               'Country':[],
               'Industry':[],
@@ -298,9 +243,10 @@ dictionary = {'Ticker':[],
               'Sector key':[]}
 
 
+# sometimes API might not connect properly and all fields become NANs
 
-# sometimes API might not connect properly
-
+# loop through the tickers we grouped before and get the company data
+# if the data isn't there then add a "nan" value
 for tick in tickers:
     
     print('now')
@@ -316,8 +262,7 @@ for tick in tickers:
             dictionary['Country'].append(info['country'])
         except:
             dictionary['Country'].append(np.nan)
-            
-        
+                   
         try:
             dictionary['Industry'].append(info['industry'])
         except:
@@ -347,31 +292,27 @@ for tick in tickers:
         dictionary['Industry key'].append(np.nan)
         dictionary['Sector key'].append(np.nan)
         
-
+# turn dictionary into dataframe
 df_info = pd.DataFrame.from_dict(dictionary)
 
 sector_data = data_other.merge(df_info, how='outer', on=['Ticker'])
-
 company_data = other.merge(sector_data, how='right', on=['Ticker'])
-
 # merge company data, group data and company name
-# on the subset of tickers we selected
+# on the subset of tickers we selected, and on the information about which "correlation group" they belong to
 
-
+# save company data offline
 company_data.to_csv('corr_company_data.csv')
-
-
 
 company_data = pd.read_csv('corr_company_data.csv')
 
 group_sizes = company_data['Corr group'].value_counts()
+# check also the sizes of the groups because it may happen that one group has almost all stocks correlated with one another
 
-# group by group and see if they tend to belong in the same country or sector
+# group tickers by "correlation group" and see if they tend to belong in the same country or sector
 
 countries = company_data[['Security Name', 'Corr group', 'Country']]
 
 sectors = company_data[['Security Name', 'Corr group', 'Sector']]
-
 
 def grouped_variables(data,variable):
 
@@ -389,6 +330,8 @@ def grouped_variables(data,variable):
 
 grouped_countries = grouped_variables(countries, 'Country')
 grouped_sectors = grouped_variables(sectors, 'Sector')
+
+# store the grouped countries and sectors in a dataframe
 
 groups_df = {'# tickers' : list(group_sizes), 
              'countries': grouped_countries, 
